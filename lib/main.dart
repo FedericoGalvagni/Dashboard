d8aa5f78ce08af808051176eae77e6a9dce281c1
FedeFedericoGalvagni <federico.galvagni.br@gmail.com>
2022-07-06 16:51:49 +0200

    2.0 mayor function are correct

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/get_navigation.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:interface_example1/classes/modelli/variabili_globali.dart';
 import 'package:interface_example1/constants/style.dart';
